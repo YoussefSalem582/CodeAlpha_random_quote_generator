b8ed4c7b082568770f2b73600a6e9007c4406b22
YoussefSalem582 <youssef.salem.hassan582@gmail.com>
2025-03-08 00:49:21 +0200

    Refactor quote fetching and add favorite quotes functionality
    - Update QuoteService to fetch quotes by category and return Quote objects
    - Add Quote model to encapsulate quote data
    - Add favorite quote functionality with a toggle button
    - Implement a screen to display favorite quotes
    - Add category selection dropdown in home screen
    - Implement animated transitions for quote changes

---
+++ lib/screens/favorite_quotes.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import '../widgets/quote_card.dart';
+import '../model/quote.dart';
+
+class FavoriteQuotesScreen extends StatelessWidget {
+  final List<Quote> _favoriteQuotes;
+
+  const FavoriteQuotesScreen({
+    super.key,
+    required List<Quote> favoriteQuotes,
+  }) : _favoriteQuotes = favoriteQuotes;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: const Text(
+          'Favorite Quotes',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromRGBO(1, 174, 185, 1.0),
+        flexibleSpace: Platform.isIOS
+            ? null
+            : Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/background.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView.builder(
+          itemCount: _favoriteQuotes.length,
+          itemBuilder: (context, index) {
+            final quote = _favoriteQuotes[index];
+            return QuoteCard(
+              quote: quote.content,
+              author: quote.author,
+              isFavorite: quote.isFavorite,
+              onFavoriteToggle: () {},
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../services/quote_service.dart';
 import '../widgets/quote_card.dart';
+import 'favorite_quotes.dart';
+import '../model/quote.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,21 +12,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _quote = 'Click the button to get a new quote!';
-  String _author = '';
+  Quote? _quote;
+  final List<Quote> _favoriteQuotes = [];
+  String? _selectedCategory;
+  final List<String> _categories = ['inspire', 'love', 'life', 'humor'];
 
   Future<void> _getNewQuote() async {
     print('Fetching new quote...');
     try {
-      final Map<String, dynamic> quote = await QuoteService.fetchRandomQuote();
+      final Quote quote = await QuoteService.fetchRandomQuote(
+        category: _selectedCategory,
+      );
       print('Quote fetched: $quote');
       setState(() {
-        _quote = quote['content'] as String;
-        _author = quote['author'] as String;
+        _quote = quote;
       });
     } catch (e) {
       print('Failed to fetch quote: $e');
     }
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_quote != null) {
+        _quote!.isFavorite = !_quote!.isFavorite;
+        if (_quote!.isFavorite) {
+          _favoriteQuotes.add(_quote!);
+        } else {
+          _favoriteQuotes.removeWhere(
+            (quote) => quote.content == _quote!.content,
+          );
+        }
+      }
+    });
   }
 
   @override
@@ -33,13 +53,25 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text(
           'Random Quote Generator',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
         backgroundColor: Color.fromRGBO(1, 174, 185, 1.0),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite, color: Colors.red),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder:
+                      (context) =>
+                          FavoriteQuotesScreen(favoriteQuotes: _favoriteQuotes),
+                ),
+              );
+            },
+          ),
+        ],
       ),
       body: Container(
         decoration: const BoxDecoration(
@@ -48,13 +80,82 @@ class _HomeScreenState extends State<HomeScreen> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Center(child: QuoteCard(quote: _quote, author: _author)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: DropdownButton<String>(
+                value: _selectedCategory,
+                hint: const Text(
+                  'Select Category',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                items: _categories.map((String category) {
+                  return DropdownMenuItem<String>(
+                    value: category,
+                    child: Text(
+                      category,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _selectedCategory = newValue;
+                  });
+                },
+                dropdownColor: Colors.grey[900],
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: AnimatedSwitcher(
+                  duration: const Duration(seconds: 1),
+                  transitionBuilder: (
+                    Widget child,
+                    Animation<double> animation,
+                  ) {
+                    return SlideTransition(
+                      position: Tween<Offset>(
+                        begin: const Offset(0, 1),
+                        end: Offset.zero,
+                      ).animate(animation),
+                      child: ScaleTransition(
+                        scale: animation,
+                        child: FadeTransition(opacity: animation, child: child),
+                      ),
+                    );
+                  },
+                  child:
+                      _quote != null
+                          ? QuoteCard(
+                            key: ValueKey<String>(_quote!.content),
+                            quote: _quote!.content,
+                            author: _quote!.author,
+                            isFavorite: _quote!.isFavorite,
+                            onFavoriteToggle: _toggleFavorite,
+                          )
+                          : const Text(
+                            'Click the button to get a new quote!',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _getNewQuote,
         tooltip: 'New Quote',
-        child: const Icon(Icons.refresh, color: Colors.white),
         backgroundColor: Colors.blueAccent,
+        child: const Icon(Icons.refresh, color: Colors.white),
       ),
     );
   }
